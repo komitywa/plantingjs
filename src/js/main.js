fb6e414d0ac731eadec6afa14f181385a074160d
Tomasz Magulski <tomasz.magulski@gmail.com>
2015-04-11 18:58:04 +0200

    #17 - providing callback for save

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,13 @@
+
+function simple_save_callback() {
+    console.log('save callback');
+}
+
 var plantingJSInstance;
 
 $(document).ready(
     function () {
       plantingJSInstance = new PlantingJS();
-      plantingJSInstance.initFromMap($("#viewport"), "/manifesto.json");
+      plantingJSInstance.initFromMap($("#viewport"), "/manifesto.json", simple_save_callback);
     }
 );
--- src/js/plantingjs-core.js
+++ src/js/plantingjs-core.js
@@ -34,14 +34,14 @@ function PlantingJS () {
     }
 };
 
-PlantingJS.prototype.initFromMap = function (div, manifesto) {
-    var pe = new Planting(div);
+PlantingJS.prototype.initFromMap = function (div, manifesto, save_callback) {
+    var pe = new Planting(div, save_callback);
     this.plantingEngineList.push(pe);
     $.getJSON(manifesto).done(pe.download_manifesto(this));
     return pe;
 };
 
-PlantingJS.prototype.initFromStreetView = function (div, manifesto) {
+PlantingJS.prototype.initFromStreetView = function (div, manifesto, save_callback) {
     var pe = new Planting(div);
     this.plantingEngineList.push(pe);
     return pe;
