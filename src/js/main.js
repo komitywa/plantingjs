642fa7c8510f120cc256ac6be839e359df2d56d0
Szymon Krolikiewicz <krolikiewicz@gmail.com>
2015-08-08 15:35:13 +0200

    Wyswietlanie dialogu (jquery-ui) po wejsciu na strone. Dialog wyswietlany w zaleznosci od ustawien  przekazanych w konstruktorze

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,8 @@
-
 var plantingJSInstance;
 
 $(document).ready(
     function () {
-      plantingJSInstance = new PlantingJS();
+      plantingJSInstance = new PlantingJS({showHelp: true});
       plantingJSInstance.initFromMap($("#viewport"), "/manifesto.json", simple_save_callback);
     }
 );
