a6fe97dfca51a020bd7b8f1b8615a3e6409f62c7
Tomasz Magulski <tomasz.magulski@gmail.com>
2015-10-27 00:51:10 +0100

    #95 - lintify

--- src/js/manifesto-data.js
+++ src/js/manifesto-data.js
@@ -1,26 +1,21 @@
-var Core = require('core');
-var Const = require('const');
+import Core from 'core';
+import Const from 'const';
 
-var ManifestoDataModel = Core.Model.extend({
+const ManifestoDataModel = Core.Model.extend({
+  constructor: function(data, options) {
+    this.url = options.url;
+    Core.Model.call(this, data, options);
+  },
 
-    constructor: function(data, options) {
+  initialize: function() {
+    this.on('fetch', function() {
+      this.app.trigger(Const.EVENT.MANIFESTO_INITED, this);
+    }, this);
+  },
 
-        this.url = options.url;
-        Core.Model.call(this, data, options);
-    },
-
-    initialize: function() {
-
-        this.on('fetch', function() {
-
-            this.app.trigger(Const.EVENT.MANIFESTO_INITED, this);
-        }, this);
-    },
-
-    getProjectionsFor: function(objectId) {
-        var toolboxobjects = this.get('toolboxobjects')[objectId];
-
-        return toolboxobjects.projections;
-    }
+  getProjectionsFor: function(objectId) {
+    const toolboxobjects = this.get('toolboxobjects')[objectId];
+    return toolboxobjects.projections;
+  },
 });
 module.exports = ManifestoDataModel;
